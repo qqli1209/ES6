9b2b5abad6726968407ff163e624905f093202f0
qqli1209 <lqq201194@163.com>
2017-09-25 21:20:12 +0800

    finish decorator

--- demo_code/Decorator.js
+++ demo_code/Decorator.js
@@ -3,26 +3,58 @@
  * Decorator.js
  */
 
-function testable(target) {
-  target.ieTestable = true;
+import {
+  autobind,
+  deprecate,
+  readonly
+} from 'core-decorators';
+
+class Person {
+  @autobind
+  getPerson() {
+    return this;
+  }
 }
 
-@testable
-class Test {
+let person = new Person();
+let getPerson = person.getPerson;
+
+console.log(getPerson() === person); // true
+
+class P {
+  @deprecate
+  facepalm() {}
+
+  @deprecate('We stopped facepalming')
+  facepalmHard() {}
+
+  @deprecate('We stopped facepalming', { url: 'http://knowyourmeme.com/memes/facepalm' })
+  facepalmHarder() {}
+}
+
+let p = new P();
+// DEPRECATION Person#facepalm: This function will be removed in future versions.
+p.facepalm();
+p.facepalmHard();
+// DEPRECATION Person#facepalmHard: We stopped facepalming
+
+p.facepalmHarder();
+// DEPRECATION Person#facepalmHarder: We stopped facepalming
+
+
+class Meal {
   @readonly
-  name() {
-    return 'lqq';
-  }
-  print() {
-    console.log('hello world');
+  test() {
+    console.log('test');
   }
 }
-
-
-
-
-
-
+var m = new Meal();
+m.test();
+/*
+m.test = function() {
+  console.log('changed'); // Cannot assign to read only property 'test' of object '#<Meal>'
+}
+*/
 
 
 
